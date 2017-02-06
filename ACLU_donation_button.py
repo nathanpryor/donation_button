00052a6e92bd797341e1c679b6f9a286f4211859
nathanpryor <nathanpryor@users.noreply.github.com>
2017-02-06 11:15:28 -0800

    Code, Read Me, and Supporting Documentation

---
+++ ACLU_donation_button.py
@@ -0,0 +1,49 @@
+import mechanize
+import boto3
+import os
+
+sns=boto3.client('sns')
+phone_number='PHONE_INCLUDING_COUNTRYCODE_AND_AREA_CODE'
+
+from base64 import b64decode
+CC_number=boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['CC_number']))['Plaintext']
+CC_expiration_month=boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['CC_expiration_month']))['Plaintext']
+CC_expiration_year=boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['CC_expiration_year']))['Plaintext']
+CC_CSC=boto3.client('kms').decrypt(CiphertextBlob=b64decode(os.environ['CC_CSC']))['Plaintext']
+
+br = mechanize.Browser(factory=mechanize.RobustFactory()) 
+
+def lambda_handler(event, context):
+	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+	br.open("https://action.aclu.org/donate-aclu")
+	br.select_form(nr=3)
+
+	br.form['submitted[donation][aclu_recurring]']=0 
+	br.form['submitted[donation][amount]']=["other"]
+	br.form['submitted[donation][other_amount]']="5"
+
+	br.form['submitted[donor_information][first_name]']="FIRST_NAME"
+	br.form['submitted[donor_information][last_name]']="LAST_NAME"
+	br.form['submitted[donor_information][email]']="EMAIL"
+
+	br.form['submitted[billing_information][address]']="ADDRESS"
+	br.form['submitted[billing_information][city]']="CITY"
+	br.form['submitted[billing_information][state]']=["STATE_CODE"] 
+	br.form['submitted[billing_information][zip]']="ZIP_CODE"
+	br.form['submitted[billing_information][country]']=["COUNTRY_CODE"] 
+
+	br.form['submitted[credit_card_information][card_number]']=CC_number
+	br.form['submitted[credit_card_information][expiration_date][card_expiration_month]']=[CC_expiration_month]
+	br.form['submitted[credit_card_information][expiration_date][card_expiration_year]']=[CC_expiration_year]
+	br.form['submitted[credit_card_information][card_cvv]']=CC_CSC
+
+	br.form['submitted[credit_card_information][fight_for_freedom][1]']=0
+	br.form['submitted[credit_card_information][profile_may_we_share_your_info][1]']=0
+
+	response = br.submit()
+	if "Thank You" in response.read():
+    	message = '$5 donated to the ACLU!'
+    	sns.publish(PhoneNumber=phone_number, Message=message)
+	else:
+		message = 'Error: no donation occurred'
+		sns.publish(PhoneNumber=phone_number, Message=message)
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # donation_button
+This Python script was a quick-and-dirty solution to build an Amazon Dash button that would donate to the ACLU through the page https://action.aclu.org/donate-aclu
+
+The script requires the Mechanize library (http://wwwsearch.sourceforge.net/mechanize/).
+
+Replace the obvious placeholders with your own information. 
+
+It's set up to be run on Amazon's AWS Lambda service, with the credit card info stored as the following encrypted environment variables for a tiny bit more security:
+	CC_number
+	CC_expiration_month
+	CC_expiration_year
+	CC_CSC
+If you're not doing that, go ahead and put them in as strings.
+
+Note that the state and country menus use numeric codes rather than abbreviations. See the file country_and_state_codes.txt for the select menus from the page so you can find the appropriate value and enter it.
